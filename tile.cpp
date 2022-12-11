73b6fb3405c44c0461cb6e75edcc8145283f4a0e
Raquel <raquelbb28.08@gmail.com>
2022-12-11 17:00:25 +0000

    done ??

--- tile.cpp
+++ tile.cpp
@@ -8,11 +8,13 @@ int columns;
 int lines;
 map<vector<int>, long int> calculatedValues;
 
+
 vector<int> create_copy(vector<int> vec)
 {
     vector<int> v(vec);
     return v;
 }
+
 
 int findRightPosition(vector<int> numbers) {
     int biggest = numbers[0];
@@ -31,6 +33,7 @@ int findRightPosition(vector<int> numbers) {
     return index;
 }
 
+
 bool checkIfAllZero(vector<int> numbers) {
     for(int i=0; i<lines; i++) {
         if(numbers[i] != 0){
@@ -40,6 +43,7 @@ bool checkIfAllZero(vector<int> numbers) {
 
     return true;
 }
+
 
 bool squareFits(vector<int> numbers, int size, int line) {
     int line1 = numbers[line];
@@ -51,62 +55,45 @@ bool squareFits(vector<int> numbers, int size, int line) {
     return true;
 }
 
+
 int findNumberOfConfig(vector<int> numbers) {
+    long int numberOfConfig = 0;
     auto itr = calculatedValues.find(numbers);
 
-    if(checkIfAllZero(numbers)){
-        return 1;
-    }
+    if (checkIfAllZero(numbers)) return 1;
 
     else if(itr != calculatedValues.end()) {
-        printf("entrou aqui\n");
         return itr -> second;
     }
 
     else{
-        long int numberOfConfig = 0;
         int line = findRightPosition(numbers);
         for(int i = 1; i <= lines; i++) {
 
             if(squareFits(numbers, i, line)) {
 
-                printf("valor de numeros: ");
-                for(int j = 0; j<lines; j++) {
-                    printf("%d  ", numbers[j]);
-                }
-                printf("\n");
-
                 vector<int> numbers2 = create_copy(numbers);
                 for(int j = line; j < line + i; j++) { // removes the square
                     numbers2[j] -= i;
                 }
-                printf("depois de tirar quadrado: ");
-                for(int j = 0; j<lines; j++) {
-                    printf("%d  ", numbers2[j]);
-                }
-                printf("\n");
 
                 numberOfConfig += findNumberOfConfig(numbers2);
-                printf("numero config: %d\n", numberOfConfig);
-                printf("quadrado que vamor adicionar com aquela config: ");
-                for(int j = 0; j<lines; j++) {
-                    printf("%d  ", numbers[j]);
-                }
-                printf("\n\n");
-                calculatedValues.insert({numbers, numberOfConfig});
             }
 
             else{
                 break;
             }
         }
+
+        calculatedValues.insert({numbers, numberOfConfig});
     }
 
-    return itr->second; 
+    return numberOfConfig; 
 }
 
+
 int main() {
-    bool allZero = true;
+
 
     scanf("%d", &lines);
     scanf("%d", &columns);
@@ -115,12 +102,9 @@ int main() {
 
     for(int i= 0; i < lines; i++){
         cin >> numbers[i];
-        if(numbers[i] != 0) allZero = false;
     }
 
-    if(lines == 0 || allZero) {
-        cout << "0\n";
-    }
+    if(lines == 0 || numbers[lines - 1] == 0) cout << "0\n";
     else {
         cout << findNumberOfConfig(numbers) << "\n";
         numbers.clear();
