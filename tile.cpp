3eab79f43c761b578d5b429f992c8dca7e79fc35
Raquel <raquelbb28.08@gmail.com>
2022-12-11 12:59:49 +0000

    trying to find a way to calculate all paths

--- tile.cpp
+++ tile.cpp
@@ -5,7 +5,35 @@ using namespace std;
 
 int columns;
 int lines;
-int numberOfConfig = 0;
+long int numberOfConfig = 0;
+int mod = (int) (1e9 + 9);
+
+long int calculateTotalPaths(int n, int m, vector< vector< int> > &mat) {
+    for(int i = 0; i < n; i++) {
+        for(int j =0; j<m; j++) {
+            cout << mat[i][j] << "  ";
+        }
+        cout << "\n";
+    }
+
+    vector<int> prev(m,0);
+    for(int i=0; i<n; i++) {
+        vector<int> cur(m,0);
+        for(int j = 0; j<m; j++) {
+            if(mat[i][j] == -1) cur[j] = 0;
+            else if(i == 0 && j == 0) cur[j] = 1;
+            else {
+                int up = 0, left = 0;
+                if(i>0) up = prev[j];
+                if(j>0) left = cur[j-1];
+                cur[j] = (up+left) %mod; 
+            }  
+        }
+
+        prev = cur;
+    }
+    return prev[m-1];
+}
 
 vector<int> create_copy(vector<int> vec)
 {
@@ -87,15 +115,23 @@ int main() {
     scanf("%d", &columns);
 
     vector<int> numbers(lines);
+    //initializes vector
+    vector<vector<int>> grid(lines, vector<int>(columns, 0));
 
     for(int i= 0; i < lines; i++){
         cin >> numbers[i];
+        for(int j=numbers[i]; j < columns; j++) {
+            grid[i][j] = -1;
+        }
     }
 
     if(lines == 0) {
         cout << "0\n";
     }
     else {
+        int paths = calculateTotalPaths(lines, columns, grid);
+        grid.clear();
+        cout << paths <<"\n";
         cout << findNumberOfConfig(numbers) << "\n";
         numbers.clear();
     }
