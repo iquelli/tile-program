edc1eedd9faf0449d4bc338ca18e219ebfbf40c6
Raquel <raquelbb28.08@gmail.com>
2022-12-11 16:33:22 +0000

    atualizado

--- tile.cpp
+++ tile.cpp
@@ -6,7 +6,6 @@ using namespace std;
 
 int columns;
 int lines;
-long int numberOfConfig = 0;
 map<vector<int>, long int> calculatedValues;
 
 vector<int> create_copy(vector<int> vec)
@@ -55,29 +54,46 @@ bool squareFits(vector<int> numbers, int size, int line) {
 int findNumberOfConfig(vector<int> numbers) {
     auto itr = calculatedValues.find(numbers);
 
-    if(itr != calculatedValues.end()) {
+    if(checkIfAllZero(numbers)){
+        return 1;
+    }
+
+    else if(itr != calculatedValues.end()) {
         printf("entrou aqui\n");
         return itr -> second;
     }
 
-    if(checkIfAllZero(numbers)){
-        itr->second += 1;
-        return itr->second;
-    }
-
     else{
+        long int numberOfConfig = 0;
         int line = findRightPosition(numbers);
         for(int i = 1; i <= lines; i++) {
 
             if(squareFits(numbers, i, line)) {
 
+                printf("valor de numeros: ");
+                for(int j = 0; j<lines; j++) {
+                    printf("%d  ", numbers[j]);
+                }
+                printf("\n");
+
                 vector<int> numbers2 = create_copy(numbers);
                 for(int j = line; j < line + i; j++) { // removes the square
                     numbers2[j] -= i;
                 }
+                printf("depois de tirar quadrado: ");
+                for(int j = 0; j<lines; j++) {
+                    printf("%d  ", numbers2[j]);
+                }
+                printf("\n");
 
-                itr->second = findNumberOfConfig(numbers2);
-                calculatedValues.insert({numbers, itr->second});
+                numberOfConfig += findNumberOfConfig(numbers2);
+                printf("numero config: %d\n", numberOfConfig);
+                printf("quadrado que vamor adicionar com aquela config: ");
+                for(int j = 0; j<lines; j++) {
+                    printf("%d  ", numbers[j]);
+                }
+                printf("\n\n");
+                calculatedValues.insert({numbers, numberOfConfig});
             }
 
             else{
